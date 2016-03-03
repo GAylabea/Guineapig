c70695d2505c1740e15d64e41bc6c0604f78b3e3
GAylabea <gbea@rocketmail.com>
2016-03-02 19:51:50 -0600

    added DOM elements

---
+++ events.js
@@ -0,0 +1,12 @@
+// Create a variable list of all the ID's you will need to work with
+var output = document.getElementById("output-target");
+var keyPress = document.getElementById("keyPress-input");
+var guineaPig = document.getElementById("guinea-pig"); 
+var buttonColor = document.getElementById("add-color"); 
+var buttonHulkify = document.getElementById("make-large");
+var buttonCapture = document.getElementById("add-border"); 
+var buttonRounded = document.getElementById("add-rounding"); 
+var header = document.getElementById("page-header");
+var title = document.getElementById("page-title"); 
+// This one is different and needs to be captured by class name
+var article = document.getElementsByclassName("article-section");
