9ed5096b961a00d6c1af01ca6f300e995fe42f65
GAylabea <gbea@rocketmail.com>
2016-03-02 21:06:47 -0600

    added variables, mouseover event listener

--- events.js
+++ events.js
@@ -8,5 +8,20 @@ var buttonCapture = document.getElementById("add-border");
 var buttonRounded = document.getElementById("add-rounding"); 
 var header = document.getElementById("page-header");
 var title = document.getElementById("page-title"); 
-// This one is different and needs to be captured by class name
-var article = document.getElementsByclassName("article-section");
+// This one is different and needs to be captured by class name - it will always be an array
+ var article = document.getElementsByClassName("article-section");
+
+// we will need event listeners including:
+// click - this will require a for loop as we will need to output different messages for each article
+// article.addEventListener("click", function() {
+//   output.innerHTML = "you clicked on the ??? of the article"
+// }
+// console.log(output);
+// mouseover
+title.addEventListener("mouseover", function() {
+  output.innerHTML = "you moved your mouse over the header"; 
+});
+
+// mouse-out
+
+
